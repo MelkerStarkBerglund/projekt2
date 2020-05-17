0e9e301f22e3826de98c2f07c0f1ba4ed686a03b
Melker Stark Berglund <melker.starkberglund@elev.ga.ntig.se>
2020-05-17 17:20:46 +0200

    Inlämning 2020/05/17
    Fick mer feedback på den estetiska delen, så har andrat lite där.
    
    Men det jag har ändrat mest på är footern inte fungerade som den skulle innan och ar fixat det lite. Den fungerar inte perfekt fortfarande men mycket bättre än innan, innan låg footern över min main hela tiden och det gör den inte längre.
    
    Gjorde inte så mycket ändringar, hade svårt att hitta movitvation till att arbeta på hemsidan.

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,28 @@
-const nav = document.querySelector('.navLinks')
-const burger = document.querySelector('.burger')
+
+    const nav = document.querySelector('.navLinks')
+    const burger = document.querySelector('.burger')
 
 function toggleMeny() {
-document.querySelector('.navLinks').classList.toggle('navActive')
-document.querySelector('.burger').classList.toggle('toggle')
+    document.querySelector('.navLinks').classList.toggle('navActive')
+    document.querySelector('.burger').classList.toggle('toggle')
 
-}
+    }
 
 document.querySelector('.burger').addEventListener('click', toggleMeny)
 
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
